5cfcde58b52e23e05f24d91917de46824a663d76
Heitor Leal <heitorlsms@gmail.com>
2024-03-14 08:32:19 -0300

    Aula sobre métodos e funções

---
+++ aula03/funcoes1.js
@@ -0,0 +1,17 @@
+function Soma () {
+
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1+n2);
+}
+
+Soma()
+
+function SomacomParametro (n1, n2) {
+    console.log(n1+n2)
+}
+
+SomacomParametro(8, 10)
+SomacomParametro(50,10)
+
